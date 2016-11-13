9a55e854d2017860aa5c79ca71caca934f04a046
Dpalazzari <drewpalazzari@hotmail.com>
2016-11-12 19:02:17 -0700

    Update game_test.rb

--- test/game_test.rb
+++ test/game_test.rb
@@ -23,28 +23,17 @@ class GameTest < Minitest::Test
     game = Game.new
     game.start_the_game
     assert_equal true, game.correct_number > 0
-  end
-
-  def test_it_stores_guessed_num_in_a_hash_with_result
-    game = Game.new
     game.start_the_game
-    correct_number = game.correct_number
-    game.guess_your_number(1)
-    game.guess_your_number(correct_number)
-    guesses = game.guesses
-    assert_equal "Your guess of 1 was too low. You have guessed 1 times.", guesses.first
+    assert rand(0..100), game.correct_number
   end
 
   def test_it_will_tell_you_your_guesses
-    skip
     game = Game.new
     game.start_the_game
-    correct_number = game.correct_number
-    game.guess_your_number(100)
-    game.guess_your_number(1)
-    game.guess_your_number(correct_number)
+    game.play_the_game
+    game.play_the_game
     guesses = game.guesses
-    assert_equal ["Your guess of 100 was too high. You have guessed 1 times.", "Your guess of 1 was too low. You have guessed 2 times."], guesses
+    assert_equal ["Your guess of 0 was too low. You have guessed 1 times.", "Your guess of 0 was too low. You have guessed 2 times."], guesses
   end
 
 end
